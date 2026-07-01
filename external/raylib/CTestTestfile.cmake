1e4da408a915d77fc8e7da0e578714ac7bb13b59
Mateus Schenato <schenatomateusfk@gmail.com>
2026-07-01 00:03:03 -0300

    Add files via upload

---
+++ external/raylib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Projetos/trabalho-final/external/raylib
+# Build directory: C:/Projetos/trabalho-final/build-web/external/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("raylib")
